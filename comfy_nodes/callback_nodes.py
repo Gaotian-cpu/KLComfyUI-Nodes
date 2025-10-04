d01e04ce6103b04d0d11bda7dd4de33a60b32800
Gaotian-cpu <52811674+Gaotian-cpu@users.noreply.github.com>
2025-10-04 17:18:40 +0800

    Add files via upload

--- comfy_nodes/callback_nodes.py
+++ comfy_nodes/callback_nodes.py
@@ -22,8 +22,12 @@ class KLCallbackVdImg(KLBasicNode):
                 "callback_url": ("STRING", {}),
                 "video": ("STRING", {}),
                 'image': ("STRING", {})
-            }
+            },
+            "hidden": {
+                "prompt_id": "PROMPT_ID",
+            },
         }
+
         return inputs
 
     def __init__(self, *args, **kwargs):
@@ -33,15 +37,13 @@ class KLCallbackVdImg(KLBasicNode):
         self.set_all_log(True)
 
     def commit_result(self, callback_url: str, video: str, image: str,
-                      prompt=None, extra_pnginfo=None, unique_id=None) -> tuple:
+                      prompt_id=None) -> tuple:
         """
         回调
         :param callback_url: 回调地址
         :param video: 视频文件路径
         :param image: 图片文件路径
-        :param prompt:
-        :param extra_pnginfo:
-        :param unique_id:
+        :param prompt_id:
         :return: (err_msg, err_code, prompt id)
         """
         if StringUtil.is_string_empty(callback_url):
@@ -56,11 +58,11 @@ class KLCallbackVdImg(KLBasicNode):
             self.__logger__.error('image path is empty, cannot commit generated result!')
             return 'image path is empty', 2, ''
 
-        # get the prompt id
-        prompt_id = self.get_prompt_id(prompt=prompt, extra_pnginfo=extra_pnginfo, unique_id=unique_id)
         if StringUtil.is_string_empty(prompt_id):
             self.__logger__.error('prompt id is empty, cannot commit generated result!')
             return 'prompt id is empty', 2, ''
+
+        self.__logger__.info('prompt id: {}'.format(prompt_id))
 
         if not FileUtil.check_file_exist(video):
             self.__logger__.error('video[{}] not exist, cannot commit generated result!'.format(video))
