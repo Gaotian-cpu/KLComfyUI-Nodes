1fd7b1ed11c49a8c86518619ed8e979a07bc44d6
Gaotian-cpu <52811674+Gaotian-cpu@users.noreply.github.com>
2025-10-04 16:45:13 +0800

    Add files via upload

---
+++ comfy_nodes/basic_nodes.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from ..klutils.klLog import Log
+from ..klutils.string_util import StringUtil
+
+
+class KLBasicNode:
+    __version__ = '1.0.0'
+    __name__ = 'KLBasicNode'
+    __logger__ = Log.get_logger(__name__)
+
+    def __init__(self, *args, **kwargs):
+        self.__prompt_id__: str = ''
+
+    def get_prompt_id(self, prompt=None, extra_pnginfo=None, unique_id=None) -> str:
+        if not StringUtil.is_string_empty(self.__prompt_id__):
+            return self.__prompt_id__
+
+        # 获取 workflow_id
+        workflow_id = None
+
+        if extra_pnginfo and 'workflow' in extra_pnginfo:
+            workflow_id = extra_pnginfo['workflow'].get('id')
+
+        if not workflow_id and prompt:
+            workflow_id = prompt.get('workflow', {}).get('id')
+
+        self.__prompt_id__ = workflow_id
+
+        return self.__prompt_id__
+
--- comfy_nodes/callback_nodes.py
+++ comfy_nodes/callback_nodes.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+from ..klutils.file_util import FileUtil
 from ..klutils.klLog import Log
 from ..klutils.string_util import StringUtil
+from .basic_nodes import KLBasicNode
 
 
-class KLCallbackVdImg:
+class KLCallbackVdImg(KLBasicNode):
     __name__ = 'KLCallbackVdImg'
     __version__ = '1.0.0'
     __logger__ = Log.get_logger(__name__)
@@ -24,12 +26,16 @@ class KLCallbackVdImg:
         }
         return inputs
 
-    def commit_result(self, callback_url: str, video: str, image: str) -> tuple:
+    def commit_result(self, callback_url: str, video: str, image: str,
+                      prompt=None, extra_pnginfo=None, unique_id=None) -> tuple:
         """
         回调
         :param callback_url: 回调地址
         :param video: 视频文件路径
         :param image: 图片文件路径
+        :param prompt:
+        :param extra_pnginfo:
+        :param unique_id:
         :return: (err_msg, err_code)
         """
         if StringUtil.is_string_empty(callback_url):
@@ -43,6 +49,19 @@ class KLCallbackVdImg:
         if StringUtil.is_string_empty(image):
             self.__logger__.error('image path is empty, cannot commit generated result!')
             return 'image path is empty', 2
+
+        if not FileUtil.check_file_exist(video):
+            self.__logger__.error('video[{}] not exist, cannot commit generated result!'.format(video))
+            return 'video[{}] not exist, cannot commit generated result!'.format(video), 2
+
+        if not FileUtil.check_file_exist(image):
+            self.__logger__.error('tail frame image[{}] not exist!'.format(image))
+
+        # get the prompt id
+        prompt_id = self.get_prompt_id(prompt=prompt, extra_pnginfo=extra_pnginfo, unique_id=unique_id)
+        if StringUtil.is_string_empty(prompt_id):
+            self.__logger__.error('prompt id is empty, cannot commit generated result!')
+            return 'prompt id is empty', 2
 
         # TODO commit the video and the image
 
