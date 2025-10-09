5f9f2f313958b854630107915ed08c2a0ea4c2fc
Gaotian-cpu <52811674+Gaotian-cpu@users.noreply.github.com>
2025-10-09 16:23:24 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 from .comfy_nodes.callback_nodes import KLCallbackVdImg
+from .comfy_nodes.misc_nodes import PromptIdFetcher
 
 NODE_CLASS_MAPPINGS = {
-    "singleVideoImgCallback": KLCallbackVdImg
+    "singleVideoImgCallback": KLCallbackVdImg,
+    'Prompt ID Fetcher': PromptIdFetcher
 }
 
 NODE_DISPLAY_NAMES_MAPPINGS = {
-    "singleVideoImgCallback": "Single Video_Img Callback"
+    "singleVideoImgCallback": "Single Video_Img Callback",
+    'Prompt ID Fetcher': 'Prompt ID Fetcher'
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAMES_MAPPINGS']
